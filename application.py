4f4cfc0b005dbd837b5ec9535d6d067a65e389b5
yoon9734 <85331736+yoon9734@users.noreply.github.com>
2023-12-07 00:48:23 +0900

    application.py

--- application.py
+++ application.py
@@ -1,33 +1,28 @@
-from flask import Flask, request, jsonify
-import sys
-sys.path.insert(0, './src')
-
+from flask import Flask, request
 import os
 import sys
 import importlib.util
-
 
 app = Flask(__name__)
 
 @app.route('/ai', methods=['GET'])
 def handle_request():
-    
     main_dir = os.path.dirname(os.path.abspath(__file__))
     main_py_dir = os.path.join(main_dir, 'src')
-    
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    src_dir = os.path.join(current_dir, 'src')
+    src_dir = os.path.join(main_dir, 'src')
 
-    # url_param.txt 파일 경로
     url_param_path = os.path.join(main_py_dir, 'url_param.txt')
+    distance_param_path = os.path.join(main_py_dir, 'distance_param.txt')
+    
     url_param = request.args.get('url')
+    distance_param = request.args.get('distance') 
     
     if url_param:
-        print(f"Extracted URL: {url_param}")
-        with open(url_param_path , 'w') as file:
+        with open(url_param_path, 'w') as file:
             file.write(url_param)
-            
-        # 다음 작업 수행
+        with open(distance_param_path, 'w') as file:
+            file.write(distance_param)
+
         os.chdir(src_dir)
         sys.path.append(src_dir)
 
@@ -35,36 +30,11 @@ def handle_request():
         spec = importlib.util.spec_from_file_location("main", src_main_path)
         main = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(main)
-        
-        # main.main()
-        
-        return f"Received URL: {url_param}"
+
+        return f"Received URL: {url_param} and Distance: {distance_param}"
     else:
         return "URL parameter not found"
 
-    # if url_param:
-    #     print(f"Extracted URL: {url_param}")
-    #     with open(url_param_path , 'w') as file:
-    #         file.write(url_param)
-            
-    #     return f"Received URL: {url_param}"
-    # else:
-    #     return "URL parameter not found"
-    
-  
-    # os.chdir(src_dir)
-
-    # sys.path.append(src_dir)
-
-    # src_main_path = os.path.join(src_dir, 'main.py')
-    # spec = importlib.util.spec_from_file_location("main", src_main_path)
-    # main = importlib.util.module_from_spec(spec)
-    # spec.loader.exec_module(main)
-    
-    # # main.main()
-
-
 if __name__ == '__main__':
-    # AWS Elastic Beanstalk에서 제공하는 환경 변수 'PORT'를 사용
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
